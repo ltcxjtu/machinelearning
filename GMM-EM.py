ef65e2cd3e6dc5bf6d379bd0f70b8be4f59c8d12
ltcxjtu <1440935236@qq.com>
2017-05-12 21:59:23 +0800

    增加一些函数

--- GMM-EM.py
+++ GMM-EM.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 data=pd.read_csv(filepath_or_buffer='西瓜数据集4-0.csv',sep=',')[['密度','含糖率']].values
 
-def caculProb(x,mean,std):
+def caculProb(x,mean,vari):
     n=len(x)
-    x=np.mat(x)
-    mean=np.mat(mean)
-    std=np.mat(std)
-    p=1/(np.power(2*np.pi,n/2)*np.linalg.det(std))*np.exp(-(x-mean)*np.linalg.pinv(std*std)*(x-mean).T/2)
+    #x=np.mat(x)
+    #mean=np.mat(mean)
+    #vari=np.mat(vari)
+    p=1/((2*np.pi)**(n/2)*np.linalg.det(np.mat(vari))**0.5)*np.exp(-np.mat(np.dot((x-mean),np.linalg.pinv(vari)))*np.mat((x-mean)).T/2)
     return float(p)
 
 k=3 #the numbers of cluster
@@ -18,6 +18,11 @@ sigma=np.zeros((k,b,b))
 for i in range(b):
     u[:,i]=np.linspace(np.amin(data,axis=0)[i],np.amax(data,axis=0)[i],num=2*k+1)[1::2]
     #u[:,i]=(np.amax(data,axis=0)[i]-np.amin(data,axis=0)[i])/2+np.amin(data,axis=0)[i]
+
+u[0,:]=data[5]
+u[1,:]=data[21]
+u[2,:]=data[26]
+
 for i in range(k):
     sigma[i,:,:]=0.1*np.eye(b)
 
@@ -25,32 +30,32 @@ caculProb(data[0],u[0],sigma[0])
 
 
 P=np.zeros((m,k))
-maxliter=40
+maxliter=100
 for q in range(maxliter):
     for j in range(m):
         for i in range(k):
             P[j,i]=alpha[i]*caculProb(data[j],u[i,:],sigma[i])
         temp=np.sum(P[j,:])
         P[j,:]=P[j,:]/temp
-    print(u)
+    print(P)
 
     for i in range(k):
         u[i,:]=np.sum([data[j]*P[j,i] for j in range(m)],axis=0)/np.sum(P[:,i])
         sigma[i]=np.sum([np.dot(np.mat(data[j]-u[i]).T,np.mat(data[j]-u[i]))*P[j,i] \
-                         for j in range(m)],axis=0)/np.sum(P[:,i])+1e-3*np.eye(b)
+                         for j in range(m)],axis=0)/np.sum(P[:,i])#+1e-3*np.eye(b)
         alpha[i]=np.sum(P[:,i])/m
 
 print('he')
-
-
-from sklearn.mixture import GaussianMixture
+print(P)
 import matplotlib.pyplot as plt
+'''
+from sklearn.mixture import GaussianMixture
 gmm=GaussianMixture(n_components=3,covariance_type='full',random_state=0)
 gmm.fit(data)
 y=gmm.predict(data)
+'''
 
-
-#y=np.argmax(P,axis=1)
+y=np.argmax(P,axis=1)
 
 X1=[]
 X2=[]
@@ -66,7 +71,7 @@ for a,b in zip(data,y):
 X1=np.array(X1)
 X2=np.array(X2)
 X3=np.array(X3)
-plt.scatter(X1[:,0],X1[:,1])
-plt.scatter(X2[:,0],X2[:,1])
-plt.scatter(X3[:,0],X3[:,1])
+plt.scatter(X1[:,0],X1[:,1],color='b')
+plt.scatter(X2[:,0],X2[:,1],color='r')
+plt.scatter(X3[:,0],X3[:,1],color='g')
 plt.show()
